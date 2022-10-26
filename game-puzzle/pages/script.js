06d7ce418c60a9dba99b80ffbaa51938a044ee57
Pavel <you@example.com>
2022-10-26 05:04:36 +0300

    feat: make full-working engine (5:04 26.10.22)

--- game-puzzle/pages/script.js
+++ game-puzzle/pages/script.js
@@ -1,5 +1,19 @@
 const items = document.querySelector('.items')
 let elementsPerString = 3
+let answer = {}
+
+
+answer = [
+    { top: 0, left: 0 },
+    { left: 100, top: 0, inner: 1 },
+    { left: 200, top: 0, inner: 2 },
+    { left: 0, top: 100, inner: 3 },
+    { left: 100, top: 100, inner: 4 },
+    { left: 200, top: 100, inner: 5 },
+    { left: 0, top: 200, inner: 6 },
+    { left: 100, top: 200, inner: 7 },
+    { left: 200, top: 200, inner: 8 }
+]
 
 const empty = {
     top: 0,
@@ -9,7 +23,7 @@ const empty = {
 const cells = []
 cells.push(empty)
 
-for (let i = 1; i < 9; i++) {
+for (let i = 1; i < elementsPerString ** 2; i++) {
     const cell = document.createElement('div')
     cell.className = 'cell'
     cell.innerHTML = i
@@ -30,17 +44,15 @@ for (let i = 1; i < 9; i++) {
 }
 
 const cell = document.querySelectorAll('.cell')
-
 console.log(cells)
+
 cell.forEach(e => {
     e.addEventListener('click', () => {
         cells.forEach(el => {
             if (el.inner === +e.innerHTML) {
-                // console.log('element', empty.left, empty.top)
                 let tempLeft = el.left
                 let tempTop = el.top
 
-                console.log() 
                 if (Math.abs((el.left - empty.left)) <= 100 && Math.abs((el.top - empty.top)) <= 100 && (Math.abs((el.top - empty.top)) !== Math.abs((el.left - empty.left)))) {
                     e.style.left = `${empty.left}px`
                     e.style.top = `${empty.top}px`
@@ -50,16 +62,23 @@ cell.forEach(e => {
 
                     empty.left = tempLeft
                     empty.top = tempTop
+
+                    let count1 = 0,
+                        count2 = 0
+                    cells.forEach(e => {
+                        if (e.inner * 100 === e.top * elementsPerString + e.left) {
+                            count1++
+                        }
+                        if (e.inner * 100-100 === e.top * elementsPerString + e.left) {
+                            count2++
+                        }
+                    })
+                    if (count1 === elementsPerString * elementsPerString - 1 || count2 === elementsPerString * elementsPerString - 1) {
+                        alert('you win')
+                    }
                 }
-                // console.log('empty', empty.left, empty.top)
             }
         })
-        // console.log(e.innerHTML)
-        // empty.left = `${left*100}px`
-        // empty.top = top
-        // e.style.top = `${empty.top*100}px`
-        // e.style.left = `${empty.left*100}px`
-
-
     })
 })
+
